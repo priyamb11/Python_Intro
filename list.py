fbb32670a90a741e0827030eca12c5643505c976
Your Name <you@example.com>
2022-12-23 18:39:17 -0500

    updated final

--- list.py
+++ list.py
@@ -1,27 +1,33 @@
 names_list = ["priyam", "puneet", "jithu"]
 print(names_list)
 
-first = "Jithu"
-if first in names_list:
+first = input("Enter the name: ")
+
+if first == "Jithu" in names_list:
     print("Hi name Jithu present in the name list")
 else:
     print("Hi name Jithu not present in the name list")
 
-second = "jithu"
-if second in names_list:
+
+second = input("Enter the name: ")
+
+if second == "jithu" in names_list:
     print("Hi name jithu present in the name list")
 else:
     print("Hi name jithu not present in the name list")
 
-third = "Jithu_test"
-if third in names_list:
+
+third = input("Enter the name: ")
+
+if third == "Jithu_test" in names_list:
     print("Hi name Jithy_test present in the name list")
 else:
     print("Hi name Jithu_test not present in the name list")
 
 names_list.append("shivey")
 names_list.remove("priyam")
+print("Updated Name List:")
 print(names_list)
 
-print(names_list[0:2])
-print(names_list[1:3])
+print("First 2 names in the list: ", names_list[0:2])
+print("Last 2 names in the list: ", names_list[1:3])
