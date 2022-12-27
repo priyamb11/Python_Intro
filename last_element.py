06b93eeed555c6fdf1e5e32382ed4721ca3cb2c0
Your Name <you@example.com>
2022-12-26 20:26:20 -0500

    needs work

---
+++ last_element.py
@@ -0,0 +1,14 @@
+li = [1,2,3,4,5,6,7]
+print(li)
+
+li = li[:-1]
+print(li)
+
+dic = {"Name": ('priyam','puneet','jithu'), "Age": ('21','46','35')}
+print(dic)
+
+ele = dic.popitem()
+print(dic)
+
+dic2 = {"Name":('priyam', 'puneet', 'jithu'), "Age":('21','46','35')}
+print(dic2)
