00517f22654a4bc19fa5cca54c9b89032492ed2e
Your Name <you@example.com>
2022-12-27 22:42:24 -0500

    needs work for age

--- dict.py
+++ dict.py
@@ -1,13 +1,14 @@
-name_dict = {"Name":('priyam','puneet','jithu','suresh'),"Age":('21', '46', '35', '18')}
+name_dict = {"Name":['priyam','puneet','jithu','suresh'],"Age":['21', '46', '35', '18']}
 print(name_dict)
 
 name = input("Enter the name: ")
 
 if name in name_dict["Name"]:
     print("Hi name", name, "is present in the name dictionary")
+   for name in name_dict["Name"][0:4] print(name_dict["Age"][0:4])
 else:
     print("Hi name", name, "not present in the name dictionary")
-    
+
 print("Number of keys in the dictionary:", len(name_dict))
 
 print("Number of elements for each key -")
