c5f659907db503328a6c3c8c2be03d15994c216a
Your Name <you@example.com>
2022-12-30 22:01:29 -0500

    final

--- prime_number.py
+++ prime_number.py
@@ -1,1 +1,11 @@
-print("hi")
+number = int(input("Number: "))
+
+if number > 1:
+    for x in range(2, int(number/2)):
+        if (number % x) == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
+else:
+    print(number, "is not a prime number")
